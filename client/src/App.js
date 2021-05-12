d8673067706bf90e7065f41d1449b073702ef62a
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-12 17:21:00 +0000

    index.html linked

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+      </Switch>
     </Router>
   );
 }
