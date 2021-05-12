367db2312f4bb1586789ddb71d67cfdd0466d1d7
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-12 17:11:55 +0000

    add assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+    </Router>
   );
 }
 
