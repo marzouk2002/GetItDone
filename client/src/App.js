dc9aee3422d81e119904cc1487ffd46e96017d30
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-13 19:39:50 +0000

    error boundary added

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,45 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Contact from './components/Contact';
 import Error from './components/Error';
 import Home from './components/Home';
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    console.log(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/contact" component={Contact}/>
-        <Route path="/" component={Error}/>
+        <ErrorBoundary>
+          <Route exact path="/" component={Home}/>
+          <Route path="/contact" component={Contact}/>
+          <Route path="/" component={Error}/>
+        </ErrorBoundary>
       </Switch>
     </Router>
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -27,8 +27,8 @@ function Home() {
                             <img src="images/pic01.jpg" alt="" />
                         </span>
                         <header className="major">
-                            <h3><Link to="landing.html" className="link">Aliquam</Link></h3>
-                            <p>Ipsum dolor sit amet</p>
+                            <h3><Link to="landing.html" className="link">Goals</Link></h3>
+                            <p>Set goals and track Progress</p>
                         </header>
                     </article>
                     <article>
@@ -36,8 +36,8 @@ function Home() {
                             <img src="images/pic02.jpg" alt="" />
                         </span>
                         <header className="major">
-                            <h3><Link to="landing.html" className="link">Tempus</Link></h3>
-                            <p>feugiat amet tempus</p>
+                            <h3><Link to="landing.html" className="link">Managment</Link></h3>
+                            <p>manage all of your projects from one place</p>
                         </header>
                     </article>
                     <article>
@@ -45,8 +45,8 @@ function Home() {
                             <img src="images/pic03.jpg" alt="" />
                         </span>
                         <header className="major">
-                            <h3><Link to="landing.html" className="link">Magna</Link></h3>
-                            <p>Lorem etiam nullam</p>
+                            <h3><Link to="landing.html" className="link">Comments & Chats</Link></h3>
+                            <p>communicate and add comments in real-time.</p>
                         </header>
                     </article>
                     <article>
@@ -54,8 +54,8 @@ function Home() {
                             <img src="images/pic04.jpg" alt="" />
                         </span>
                         <header className="major">
-                            <h3><Link to="landing.html" className="link">Ipsum</Link></h3>
-                            <p>Nisl sed aliquam</p>
+                            <h3><Link to="landing.html" className="link">Docs</Link></h3>
+                            <p>organize all documents needed</p>
                         </header>
                     </article>
                 </section>
@@ -64,11 +64,11 @@ function Home() {
                 <section id="two">
                     <div className="inner">
                         <header className="major">
-                            <h2>Massa libero</h2>
+                            <h2>About</h2>
                         </header>
-                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                        <p>Get It Done is a project management system. Its goal is to help teams getting work done as fast and effectively as possible. And it does that by letting each member know what he should do and when to do it. By giving employers and project managers the ability to assign tasks to developers and track their progress.</p>
                         <ul className="actions">
-                            <li><Link to="landing.html" className="button next">Get Started</Link></li>
+                            <li><Link to="/register" className="button next">Get Started</Link></li>
                         </ul>
                     </div>
                 </section>
