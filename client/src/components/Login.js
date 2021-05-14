7e8489461545c0d55410e7256fffd755f242011a
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-14 22:20:48 +0000

    multer installed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Form from './layout/Form'
+import Header from './layout/Header'
+import Options from './layout/Options'
 
 function Login() {
+    const [ selectedTitle, setSelectedTitle ] = useState(null)
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header/>
+            <header className="major" style={titleStyle}>
+                    <h1>Login</h1>
+            </header>
+            <main>
+                { selectedTitle ? <Form selectedTitle={selectedTitle}  setSelectedTitle={setSelectedTitle} register={false} /> : <Options register={false} setSelectedTitle={setSelectedTitle}/> } 
+            </main>
+        </>
     )
+}
+
+const titleStyle = {
+    margin: '5vmin auto',
+    textAlign: 'center'
 }
 
 export default Login
