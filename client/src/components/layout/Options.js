5f3fa416e44bce00b9899bc27f27b604b3f7529e
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-15 15:12:13 +0000

    modify class names

--- client/src/components/layout/Options.js
+++ client/src/components/layout/Options.js
@@ -17,7 +17,7 @@ function Options({ setSelectedTitle, register }) {
                                 animate={{opacity: 1, x:0}}
                                 transition={{delay: 0.2, duration: 0.8}} className="option">
                         <h2>Admin</h2>
-                        <FontAwesomeIcon className='user-icon' icon={faUserAlt}/>
+                        <FontAwesomeIcon className='user-icon admin' icon={faUserAlt}/>
                         <p>{ register ? 'Create an account' : 'Login'} as an Admin</p>
                         <button className="button primary" onClick={handleChoice} value="Admin">{ register ? 'Create' : 'Login'}</button>
                     </motion.div>
@@ -27,7 +27,7 @@ function Options({ setSelectedTitle, register }) {
                                 animate={{opacity: 1, x:0}}
                                 transition={{delay: 0.3, duration: 0.7}} className="option">
                         <h2>Project Manager</h2>
-                        <FontAwesomeIcon className='user-icon' icon={faUserAlt}/>
+                        <FontAwesomeIcon className='user-icon manager' icon={faUserAlt}/>
                         <p>{ register ? 'Create an account' : 'Login'} as a Project Manager</p>
                         <button className="button primary" onClick={handleChoice} value="Project Manager">{ register ? 'Create' : 'Login'}</button>
                     </motion.div>
@@ -37,7 +37,7 @@ function Options({ setSelectedTitle, register }) {
                                 animate={{opacity: 1, x:0}}
                                 transition={{delay: 0.4, duration: 0.6}} className="option">
                         <h2>Developer</h2>
-                        <FontAwesomeIcon className='user-icon' icon={faUserAlt}/>
+                        <FontAwesomeIcon className='user-icon developer' icon={faUserAlt}/>
                         <p>{ register ? 'Create an account' : 'Login'} as a Developer</p>
                         <button className="button primary" onClick={handleChoice} value="Developer">{ register ? 'Create' : 'Login'}</button>
                     </motion.div>
