42f540a7ca035cb703b4bae98c9a86581a5cbeaa
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-15 21:14:05 +0000

    styling the alert

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -6,9 +6,13 @@ function Alert({ msgs, deleteMsg }) {
     return (
         <div className="alerts-wrap">
             {msgs.map((msg, i) => {
-                return (<div className="alert" key={i}>
-                    <p>{msg}</p>
-                    <button onClick={()=>{deleteMsg(i)}}><FontAwesomeIcon icon={faTimes} /></button>
+                return (<div className="row alert" key={i}>
+                    <div className='col-9 col-12-medium'>
+                        <p>{msg}</p>
+                    </div>
+                    <div className='col-3 col-12-medium'>
+                        <button onClick={()=>{deleteMsg(i)}} className="alert-btn"><FontAwesomeIcon icon={faTimes}/></button>
+                    </div>
                 </div>)
             })}
         </div>
