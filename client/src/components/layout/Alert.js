687a0b366cdbea98140005f198a08e924757570d
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-16 17:19:16 +0100

    animate alerts

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
 
 function Alert({ msgs, deleteMsg }) {
     return (
         <div className="alerts-wrap">
             {msgs.map((msg, i) => {
-                return (<div className={"alert alert-"+msg.type} key={i}>
+                return (
+                <motion.div initial={{opacity:0, scale: 0.5}}
+                            animate={{opacity: 1, scale:1}}
+                            transition={{delay: 0.1, duration: 0.6}} 
+                            className={"alert alert-"+msg.type} key={i}>
                     <div className= 'alert-msg'>
                         <p>{msg.text}</p>
                     </div>
@@ -15,7 +20,7 @@ function Alert({ msgs, deleteMsg }) {
                         <FontAwesomeIcon icon={faTimes}/>
                         </div>
                     </div>
-                </div>)
+                </motion.div>)
             })}
         </div>
     )
