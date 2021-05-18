72851a98a8eeee2f31d9b03e6936e376e1500e53
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-18 15:22:24 +0100

    link to each athere

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,18 +6,22 @@ import Options from './layout/Options'
 
 function Login() {
     const location = useLocation()
-    const { msgs, selected }= location.state
+    const msgs = location.state?.msgs
+    const selected = location.state?.selected
     const [ selectedTitle, setSelectedTitle ] = useState(selected)
 
     return (
         <>
             <Header/>
-            <header className="major" style={titleStyle}>
-                    <h1>Login</h1>
-            </header>
             <main>
+                <header className="major" style={titleStyle}>
+                        <h1>Login</h1>
+                </header>
                 { selectedTitle ? <Form selectedTitle={selectedTitle}  setSelectedTitle={setSelectedTitle} register={false} msgsProp={msgs}/> : <Options register={false} setSelectedTitle={setSelectedTitle}/> } 
             </main>
+            <footer className='center_stuf'>
+                <p>No acoount? <a href='/register'>Create one now!</a></p>
+            </footer>
         </>
     )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Form from './layout/Form'
 import Header from './layout/Header'
 import Options from './layout/Options'
@@ -9,11 +10,14 @@ function Register() {
     return (
         <>
             <Header/>
-            <header className="major" style={titleStyle}>
-                    <h1>Create an account</h1>
-            </header>
             <main>
+                <header className="major" style={titleStyle}>
+                        <h1>Create an account</h1>
+                </header>
                 { selectedTitle ? <Form selectedTitle={selectedTitle}  setSelectedTitle={setSelectedTitle} register={true}/> : <Options register={true} setSelectedTitle={setSelectedTitle}/> } 
+                <footer className='center_stuf'>
+                    <p>Have an acoount? <a href='/login'>login now!</a></p>
+                </footer>
             </main>
         </>
     )
