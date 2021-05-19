6e5abc56e60f6360be12bf07e5c1f8d957d488ef
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-19 21:37:22 +0100

    profile card created

--- client/src/components/layout/ProfileCard.js
+++ client/src/components/layout/ProfileCard.js
@@ -1,12 +1,15 @@
 import React from 'react'
 // redux
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 function ProfileCard() {
     const userInfo = useSelector(state => state.userInfo)
 
     return (
         <div>
+            <div className='image'>
+                <img src={'http://localhost:5000' + userInfo.picture} alt="profile" />
+            </div>
             
         </div>
     )
