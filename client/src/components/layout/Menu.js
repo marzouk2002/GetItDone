9e1ab3e3487f8d1bfaeb344c4ad00248f9cdd32e
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-19 21:32:55 +0100

    logout functionality added

--- client/src/components/layout/Menu.js
+++ client/src/components/layout/Menu.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
+import ProfileCard  from './ProfileCard'
+// redux 
+import { useSelector, useDispatch } from 'react-redux'
+import { setInfo, isLogged } from '../../actions'
+
 
 function Menu() {
+    const login = useSelector(state => state.login)
+    const dispatch = useDispatch()
+
+    const logoutFunc = () => {
+        dispatch(setInfo(false))
+        dispatch(isLogged(false))
+    }
+
     return (
         <nav id="menu">
             <ul className="links">
@@ -10,8 +23,19 @@ function Menu() {
                 <li><NavLink activeClassName='active-link' to="/contact">Contact us</NavLink></li>
             </ul>
             <ul className="actions stacked">
-                <li><Link to="/register" className="button primary fit">Get Started</Link></li>
-                <li><Link to="/login" className="button fit">Log In</Link></li>
+                {
+                   login ?  
+                    <>
+                        <li><Link to="/" onClick={logoutFunc} className="button fit">Log Out</Link></li>
+                        <li><Link to="/profile" className="button primary fit"><ProfileCard /></Link></li>
+                    </> : 
+                    <>
+                        <li><Link to="/register" className="button primary fit">Get Started</Link></li>
+                        <li><Link to="/login" className="button fit">Log In</Link></li>
+                    </>
+
+                    
+                }
             </ul>
         </nav>
     )
---
+++ client/src/components/layout/ProfileCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// redux
+import { useSelector, useDispatch } from 'react-redux'
+
+function ProfileCard() {
+    const userInfo = useSelector(state => state.userInfo)
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProfileCard
