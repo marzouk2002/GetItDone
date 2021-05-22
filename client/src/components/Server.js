42c41938c9b70ed50608ec60cc0e62cf51b0d7d1
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-22 16:12:47 +0100

    server is displayed

--- client/src/components/Server.js
+++ client/src/components/Server.js
@@ -4,15 +4,20 @@ import { useSelector } from 'react-redux'
 
 function Server() {
 
-    const userInfo = useSelector(state => state.userInfo?.role)
+    const userInfo = useSelector(state => state.userInfo)
 
     return (
         <main>
             <header className="major" style={{marginLeft: '10%'}}>
-                    <h1>Profile</h1>
+                    <h1>Server</h1>
             </header>
+            <section className="inner" style= {inner_style}>
+                <h3>Server Id: <h3>{userInfo.serverId}</h3></h3>
+            </section>
         </main>
     )
 }
+
+const inner_style = {padding: "1rem 0"}
 
 export default Server
