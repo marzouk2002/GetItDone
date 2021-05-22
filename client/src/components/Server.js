11f30a96ba9d7bffdc482191582fbbf2a075da42
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-22 16:31:05 +0100

    admin info completed

--- client/src/components/Server.js
+++ client/src/components/Server.js
@@ -12,7 +12,49 @@ function Server() {
                     <h1>Server</h1>
             </header>
             <section className="inner" style= {inner_style}>
-                <h3>Server Id: <h3>{userInfo.serverId}</h3></h3>
+                <div>
+                    <h3 style={{marginBottom:'0', lineHeight: 'normal'}}>Server Id: </h3>
+                    <h2 style={{marginLeft:'4rem'}}>{userInfo.serverId}</h2>
+                </div>
+                <div>
+                    <h3>Admin</h3>
+                    <div class="table-wrapper">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{userInfo.name}</td>
+                                    <td>{userInfo.email}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div>
+                    <h3>Managers</h3>
+                    <div class="table-wrapper">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{userInfo.name}</td>
+                                    <td>{userInfo.email}</td>
+                                    <td>{userInfo.role}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </section>
         </main>
     )
