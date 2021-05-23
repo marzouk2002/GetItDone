038b14132aad9b251e3720ef0bbcff1c44b8953a
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-23 14:37:35 +0100

    change some stuf in the server info route

--- routes/users.js
+++ routes/users.js
@@ -224,27 +224,31 @@ router.get('/serverinfo', passport.authenticate('jwt', { session: false }), asyn
     
     const requests = []
 
-    await Promise.all(managers.map(async (man,i) => {
-        const { id, auth } = man 
-        const manager = await Users.findOne({ _id: id})
-        if(auth) {
-            managers[i] = manager
-        } else {
-            requests.push(manager)
-            managers[i] = null
-        }
-    }));
+    await Promise.all(
+        managers.map(async (man,i) => {
+            const { id, auth } = man 
+            const manager = await Users.findOne({ _id: id})
+            if(auth) {
+                managers[i] = manager
+            } else {
+                requests.push(manager)
+                managers[i] = null
+            }
+        })
+    );
 
-    await Promise.all(developers.map(async (dev, i) => {
-        const { id, auth } = dev 
-        const developer = await Users.findOne({ _id: id})
-        if(auth) {
-            developers[i] = developer
-        } else {
-            requests.push(developer)
-            developers[i] = null
-        }
-    }));
+    await Promise.all(
+        developers.map(async (dev, i) => {
+            const { id, auth } = dev 
+            const developer = await Users.findOne({ _id: id})
+            if(auth) {
+                developers[i] = developer
+            } else {
+                requests.push(developer)
+                developers[i] = null
+            }
+        })
+    );
 
     managers = managers.filter(man => man !== null)
     developers = developers.filter(dev => dev !== null)
