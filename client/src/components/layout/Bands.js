97299bd76161027fd129c322c606b722f6fa0fd0
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-24 23:41:05 +0100

    band 1 hover effect complited

--- client/src/components/layout/Bands.js
+++ client/src/components/layout/Bands.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 function Bands() {
+    
+    const mounseEntB1 = () => {
+        document.querySelector('.main').classList.add("open-band-1")
+    }
+
+    const mounseLeavB1 = () => {
+        document.querySelector('.main').classList.remove("open-band-1")
+    }
+
     return (
         <div className='band-wrapper'>
-            <p>logout</p>
+            <div onMouseEnter={mounseEntB1} onMouseLeave={mounseLeavB1} className="band-1">
+                <div className="band-1-content">
+                    <p>Account</p>
+                </div>
+            </div>
         </div>
     )
 }
