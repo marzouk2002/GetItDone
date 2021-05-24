e0b3bf935e5192e845c0e95b2b296e7cca287e09
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-24 22:19:08 +0100

    band componet created

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Redirect } from 'react-router'
+// components
+import Bands from './layout/Bands'
 // redux
 import { useSelector } from 'react-redux'
 
@@ -8,12 +10,13 @@ function Dashboard() {
     const login = useSelector(state => state.login)
 
     return (
-        <>
-         { !login && <Redirect to={{pathname: "/login", state: {msgs:[{text: 'Sorry you should logged in to access that page',type:"danger" }]}}} /> }
-        <div>
-            <h1>Dashboard</h1>
+        <div className="dashbord-container">
+            { !login && <Redirect to={{pathname: "/login", state: {msgs:[{text: 'Sorry you should logged in to access that page',type:"danger" }]}}} /> }
+            <Bands/>
+            <div className="main">
+                <h1>Dashboard</h1>
+            </div>
         </div>
-        </>
     )
 }
 
---
+++ client/src/components/layout/Bands.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Bands() {
+    return (
+        <div className='band-wrapper'>
+            <p>logout</p>
+        </div>
+    )
+}
+
+export default Bands
