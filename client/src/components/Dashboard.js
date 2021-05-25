d49f9c629468010a84e1ac497f2f21811067bf14
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-25 20:48:32 +0100

    setserverinfo to the store from the dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Redirect } from 'react-router'
 // components
 import SideMenu from './layout/SideMenu'
 import ProjectsMenu from './layout/ProjectsMenu'
 // redux
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { setServerInfo as setServerStore } from '../actions'
 // style 
 import '../dashboard.css'
 
 
 function Dashboard() {
-
+    const dispatch = useDispatch()
     const login = useSelector(state => state.login)
+
+    const [ serverInfo, setServerInfo ] = useState(useSelector(state => state.serverInfo))
+
+    const token = localStorage.getItem('token')
+    
+    useEffect(()=> {
+        if(!serverInfo) {
+            fetch('http://localhost:5000/users/serverinfo', {
+                headers : { Authorization: token }
+            })
+            .then(res => res.json())
+            .then(data => {
+                setServerInfo(data.serverInfo)
+                dispatch(setServerStore(data.serverInfo))
+            })
+        }
+    }, [ token, dispatch, serverInfo ])
 
     return (
         <div className="dashbord-container">
