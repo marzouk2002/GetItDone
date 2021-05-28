8046cdcc65817ab7d39b292b30e298a2c514c79a
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-28 15:46:37 +0100

    adding state to the project form

--- client/src/components/layout/dashboard/ProjectForm.js
+++ client/src/components/layout/dashboard/ProjectForm.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+//redux
+import { useSelector, useDispatch } from 'react-redux'
+import { setServerInfo } from '../../../actions'
 
 function ProjectForm() {
+    const [ developers, setDevelopers ] = useState([])
+    const [ managers, setManagers ] = useState([])
+
+    const dispatch = useDispatch()
+    const serverInfo = useSelector(state => state.serverInfo)
+    const token = localStorage.getItem('token')
+
+    useEffect(()=> {
+        fetch('http://localhost:5000/app/serverinfo', {
+            headers : { Authorization: token }
+        })
+        .then(res => res.json())
+        .then(data => {
+            const { developers, managers } = data.serverInfo
+            setDevelopers(developers)
+            setManagers(managers)
+            dispatch(setServerInfo(data.serverInfo))
+        })
+    }, [ token, dispatch, serverInfo ])
+
     return (
         <div>
             <h1>Start a Project</h1>
