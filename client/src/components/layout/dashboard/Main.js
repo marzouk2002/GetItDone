533f89a71457ae93417b6d35271a4f08166e04fb
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-05-29 21:31:59 +0100

    loading state done

--- client/src/components/layout/dashboard/Main.js
+++ client/src/components/layout/dashboard/Main.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 //components 
 import ProjectForm from './ProjectForm'
+import Loading from '../Loading'
 
 export default function Main({ projects ,selectedIndex }) {
     const projectSelect = projects[selectedIndex]
-
+    const [loading, setLoading] = useState(false)
     return (
         <div className="major pro-main">
             {
                 projectSelect ? <><h1>{projectSelect.title}</h1><h4>{projectSelect.description}</h4> </>
                 : <ProjectForm/>
             }
+            {loading && <Loading/>}
         </div>
     )
 }
