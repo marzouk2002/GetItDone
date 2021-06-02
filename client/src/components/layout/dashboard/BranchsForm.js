7e10022f09a45dd7b4080dfd0c8b7001b61bc7f7
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-02 19:31:06 +0100

    adding the task

--- client/src/components/layout/dashboard/BranchsForm.js
+++ client/src/components/layout/dashboard/BranchsForm.js
@@ -1,23 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 // CKEditor
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
 function BranchsForm() {
+    const [ newTask, setNewTask ] = useState('')
+    const [ tasksArr, setTaskArr ] = useState([])
+
+    const handleNewTask = (e) => {
+        setNewTask(e.target.value)
+    } 
+
+    const addTask = (e) => {
+        e.preventDefault()
+        setTaskArr(newTask ? [...tasksArr, newTask] : [...tasksArr])
+        setNewTask('')
+    }
+
+    const resetBtn = () => {
+        setTaskArr([])
+        setNewTask('')
+    }
+
     return (
         <div className="center_stuf">
             <h4>Add a branch</h4>
-            <form  style={{width: '80%'}}>
+            <form style={{width: '80%'}}>
                 <div className="field" style={{marginBottom: '1.5rem'}}>
                     <label htmlFor="title">Title</label>
                     <input type="text" name="title" required/>
                 </div>
-                <div className="field">
+                <div className="field" style={{marginBottom: '1.5rem'}}>
                     <label htmlFor="description">Description</label>
                     <CKEditor
                         editor={ ClassicEditor }
                         />
                 </div>  
+                <div className="field" style={{marginBottom: '2rem'}}>
+                    <label htmlFor="tasks">Tasks</label>
+                    <ul style={{fontSize: '1.2rem', marginBottom: '1.2rem'}}>
+                        { tasksArr.map((task, i)=><li key={i}>{task}</li>) }
+                    </ul>
+                    <div style={{display: 'flex'}}>
+                        <input type="text" name="tasks" style={{flex:'100% 1 1'}} value={newTask} onChange={handleNewTask}/>
+                        <button className='btn primary' onClick={addTask}>Add</button>
+                    </div>
+                </div>
+                <ul className="actions" style={{ justifyContent: 'center'}}>
+                    <button className="button" type='reset' onClick={resetBtn}>Reset</button>
+                    <button className="button primary" style={{marginLeft:'4rem'}}>Submit</button>
+                </ul>
             </form>
         </div>
     )
