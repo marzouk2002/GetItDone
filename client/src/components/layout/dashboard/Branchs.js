5a22ade176ca68e4f98b9ba60d36d8265fdfcfd2
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-03 16:44:56 +0100

    delete btn styled and markup

--- client/src/components/layout/dashboard/Branchs.js
+++ client/src/components/layout/dashboard/Branchs.js
@@ -3,6 +3,10 @@ import React from 'react'
 import BranchsForm from './BranchsForm'
 // redux
 import { useSelector } from 'react-redux'
+// motion & fontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
 
 function Branchs({ branchs, projectId, setUpDate }) {
     const { role } = useSelector(state => state.userInfo)
@@ -10,6 +14,16 @@ function Branchs({ branchs, projectId, setUpDate }) {
     return (
         <div>
             <label>Branchs</label>
+            {
+                branchs.map((branch, i) => {
+                    return (<div key={i} className="branch-card">
+                        <h4>{branch.title}</h4>
+                        <div className="delete-btn" title='delete branch'>
+                            <FontAwesomeIcon icon={faTimes}/>
+                        </div>
+                    </div>)
+                })
+            }
             { role==='manager' && <BranchsForm projectId={projectId} setUpDate={setUpDate}/> }
         </div>
     )
