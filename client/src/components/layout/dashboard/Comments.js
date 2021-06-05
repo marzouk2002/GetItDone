d3ba85632c57b705b6e52e31e7390ab2b197918c
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-05 15:23:02 +0100

    comment form done

--- client/src/components/layout/dashboard/Comments.js
+++ client/src/components/layout/dashboard/Comments.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 function Comments() {
     return (
-        <div>
+        <div className="comments-cont">
             <label>comments</label>
+            <form>
+                <div className="field">
+                    <input type="text" name="tasks" placeholder="Add a comment" style={{flex:'100% 1 1'}}/>
+                    <input className='btn primary' value="Add" type="submit"/>
+                </div>
+            </form>
         </div>
     )
 }
