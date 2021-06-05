4e1632c51985dd1cb0e56fd5d1db8900a7732708
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-05 19:36:31 +0100

    delete comments done

--- client/src/components/layout/dashboard/Comments.js
+++ client/src/components/layout/dashboard/Comments.js
@@ -32,8 +32,7 @@ function Comments({ comments, projectId, setUpDate}) {
             method: 'DELETE',
             body: JSON.stringify({ commentId, projectId }),
             headers : { "Authorization": token, "Content-Type" : "application/json" }
-        }).then(res => res.json())
-        .then(data => console.log(data))
+        }).then(res => setUpDate(Math.random()*10000))
         .catch(err => console.log(err))
     }
  
@@ -46,28 +45,30 @@ function Comments({ comments, projectId, setUpDate}) {
                     <input className='btn primary' value="Add" type="submit"/>
                 </div>
             </form>
-            <div className="comments-cont">
-                {
-                    comments.map((comment, i) => (
-                        <div key={i} className="comment">
-                            <div className='image'>
-                            { comment.picture ? <img src={'http://localhost:5000' + comment.picture} alt="profile" /> :
-                            <FontAwesomeIcon className={`user-icon user-icon-small ${comment.role}`}  icon={faUserAlt}/>} 
-                            </div>
-                            <div>
-                                <h4>{comment.userName}</h4>
-                                <p>{comment.content}</p>
-                            </div>
-                            {
-                                _id === comment.userId &&
-                                <div className="delete-btn" title='delete comment' onClick={()=>deleteComment(comment.id, projectId)}>
-                                        <FontAwesomeIcon icon={faTimes}/>
+            {
+              comments.length ?  <div className="comments-cont">
+                    {
+                        comments.map((comment, i) => (
+                            <div key={i} className="comment">
+                                <div className='image'>
+                                { comment.picture ? <img src={'http://localhost:5000' + comment.picture} alt="profile" /> :
+                                <FontAwesomeIcon className={`user-icon user-icon-small ${comment.role}`}  icon={faUserAlt}/>} 
                                 </div>
-                            }
-                        </div>
-                    ))
-                }
-            </div>
+                                <div>
+                                    <h4>{comment.userName}</h4>
+                                    <p>{comment.content}</p>
+                                </div>
+                                {
+                                    _id === comment.userId &&
+                                    <div className="delete-btn" title='delete comment' onClick={()=>deleteComment(comment.id, projectId)}>
+                                            <FontAwesomeIcon icon={faTimes}/>
+                                    </div>
+                                }
+                            </div>
+                        ))
+                    }
+                </div> : ''
+            }
         </div>
     )
 }
