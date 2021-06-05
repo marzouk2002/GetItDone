e2495b8390412b2d23b2d562a79efaed41bf4add
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-05 15:29:03 +0100

    state hooked with form input

--- client/src/components/layout/dashboard/Comments.js
+++ client/src/components/layout/dashboard/Comments.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Comments() {
+    const [ formInput, setFormInput] = useState('')
+
+    const handleInputChange = (e) => {
+        setFormInput(e.target.value)
+    }
+ 
     return (
         <div className="comments-cont">
             <label>comments</label>
             <form>
                 <div className="field">
-                    <input type="text" name="tasks" placeholder="Add a comment" style={{flex:'100% 1 1'}}/>
+                    <input type="text" name="tasks" placeholder="Add a comment" value={formInput} onChange={handleInputChange}/>
                     <input className='btn primary' value="Add" type="submit"/>
                 </div>
             </form>
