ace67b79d011465954285d793718ce9b34faff36
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-06 14:19:59 +0100

    slide messages done

--- client/src/components/layout/dashboard/Texting.js
+++ client/src/components/layout/dashboard/Texting.js
@@ -4,14 +4,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComments, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 function Texting() {
+    const textContainer = document.querySelector('.texting-cont')
+
     return (
         <div className='texting'>
-            <div className='btn'>
+            <div className='btn' onClick={()=>{textContainer.classList.remove('close')}} title='Messages'>
                 <FontAwesomeIcon icon={faComments} />
             </div>
-            <div className="texting-cont">
+            <div className="texting-cont close">
                 <header>
-                    <div className='btn'>
+                    <div className='btn' onClick={()=>{textContainer.classList.add('close')}}>
                         <FontAwesomeIcon icon={faChevronRight} />
                     </div>
                     <h2>Messages</h2>
