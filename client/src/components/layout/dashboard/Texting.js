f7698fc896f632e39e73d5f2bc885766615b442c
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-06 14:12:38 +0100

    texting container done

--- client/src/components/layout/dashboard/Texting.js
+++ client/src/components/layout/dashboard/Texting.js
@@ -1,13 +1,21 @@
 import React from 'react'
 // font-awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComments } from '@fortawesome/free-solid-svg-icons'
+import { faComments, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 
 function Texting() {
     return (
         <div className='texting'>
             <div className='btn'>
                 <FontAwesomeIcon icon={faComments} />
+            </div>
+            <div className="texting-cont">
+                <header>
+                    <div className='btn'>
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </div>
+                    <h2>Messages</h2>
+                </header>
             </div>
         </div>
     )
