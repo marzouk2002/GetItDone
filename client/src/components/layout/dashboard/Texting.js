9225ee52acb13c283da2b377443c57a487629a64
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-09 12:54:58 +0100

    contact and conversations added

--- client/src/components/layout/dashboard/Texting.js
+++ client/src/components/layout/dashboard/Texting.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react'
 // font-awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComments, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { faComments, faArrowLeft, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function Texting() {
     const [ msgInpu, setMsgInpu ] = useState('')
     const textContainer = document.querySelector('.texting-cont')
+
     const handleChange = (e) => {
         setMsgInpu(e.target.value)
+    }
+
+    const openConv = () => {
+        document.querySelector('.conversation').classList.toggle('open')
     }
 
     return (
@@ -18,14 +23,26 @@ function Texting() {
             <div className="texting-cont close">
                 <header>
                     <div className='btn' onClick={()=>{textContainer.classList.add('close')}}>
-                        <FontAwesomeIcon icon={faChevronRight} />
+                        <FontAwesomeIcon icon={faTimes} />
                     </div>
                     <h2>Messages</h2>
                 </header>
-                <form>
-                    <input type="text" name='message' value={msgInpu} onChange={handleChange}/>
-                    <input type="submit" value='Send' className='button primary'/>
-                </form>
+                <main>
+                    <div className="contacts">
+                        <h4 onClick={openConv}>contacts</h4>
+                    </div>
+                    <div className="conversation">
+                        <header>
+                            <div className='btn' onClick={openConv}>
+                                <FontAwesomeIcon icon={faArrowLeft}/>
+                            </div>
+                        </header>
+                        <form>
+                            <input type="text" name='message' value={msgInpu} onChange={handleChange}/>
+                            <input type="submit" value='Send' className='button primary'/>
+                        </form>
+                    </div>
+                </main>
             </div>
         </div>
     )
