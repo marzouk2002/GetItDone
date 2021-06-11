c056115ae951d5a070ea6528e7d0a880b3c53a9b
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-11 21:42:08 +0100

    using selected id in texting

--- client/src/components/layout/dashboard/Texting.js
+++ client/src/components/layout/dashboard/Texting.js
@@ -11,8 +11,9 @@ const socket = socketIOClient("http://localhost:5000");
 function Texting() {
     const [ contacts, setContacts ] = useState([])
     const [ conversations, setConversations ] = useState([])
-    const [ selectedConv, setSelectConv ] = useState([])
-    const [ selectedContact, setSelectCont ] = useState(null)
+    const [ selectedId, setSelectedId ] = useState("")
+    const [ targetContact, setTargetCont ] = useState(null)
+    const [ targetConv, setTargetConv ] = useState([])
     const [ msgInpu, setMsgInpu ] = useState('')
     const {_id, serverId} = useSelector(state => state.userInfo)
     const textContainer = document.querySelector('.texting-cont')
@@ -25,37 +26,31 @@ function Texting() {
         })
 
         socket.on('reseve-msg', ({ message }) => {
-            // setConversations({...conversations, [message.userId]:  [...conversations[message.userId], message]})
-            setSelectConv([...selectedConv, message])
+            console.log(message)
         })
     }, [])
-    
+
     useEffect(() => {
-        const newConvs = {}
-        contacts.forEach(contact => {
-            newConvs[contact._id] = []
-        })
-        setConversations(newConvs)
-    }, [contacts])
+        setTargetCont(contacts.find(contact => contact._id === selectedId))
+        setTargetConv(conversations.find(contact => contact.userid === selectedId))
+    }, [ selectedId ])
     
     const handleChange = (e) => {
         setMsgInpu(e.target.value)
     }
 
-    const openConv = (index) => {
+    const openConv = (id) => {
         document.querySelector('.conversation').classList.add('open')
-        setSelectCont(contacts[index])
-        // setSelectConv(conversations[contacts[index]._id])
+        setSelectedId(id)
     }
 
     const closeConv = () => {
         document.querySelector('.conversation').classList.remove('open')
-        // setSelectConv(null)
     }
 
     const sendMessage = (e) => {
         e.preventDefault()
-        socket.emit('send-message', {serverId, sentFrom: _id, sentTo: selectedContact._id, message: msgInpu})
+        socket.emit('send-message', {serverId, sentFrom: _id, sentTo: selectedId, message: msgInpu})
         setMsgInpu('')
         e.target.elements.message.focus()
     }
@@ -76,7 +71,7 @@ function Texting() {
                     <div className="contacts">
                         {
                             contacts.map((contact, i) => (
-                                <div key={i} className="contact" onClick={() =>openConv(i)}>
+                                <div key={i} className="contact" onClick={() =>openConv(contact._id)}>
                                     { contact.picture ? <img src={'http://localhost:5000' + contact.picture} alt="profile" /> :
                                     <FontAwesomeIcon className={`user-icon user-icon-small ${contact.role}`}  icon={faUserAlt}/>} 
                                     <h4>{contact.name}</h4>
@@ -90,17 +85,17 @@ function Texting() {
                             <div className='btn' onClick={closeConv}>
                                 <FontAwesomeIcon icon={faArrowLeft}/>
                             </div>
-                            {selectedContact && <div className='target-info'>
-                                { selectedContact.picture ? <img src={'http://localhost:5000' + selectedContact.picture} alt="profile" /> :
-                                    <FontAwesomeIcon className={`user-icon user-icon-small ${selectedContact.role}`}  icon={faUserAlt}/>} 
-                                    <h4>{selectedContact.name}</h4>
-                                    {selectedContact.online && <div className='online-dot'></div>}
+                            {targetContact && <div className='target-info'>
+                                { targetContact.picture ? <img src={'http://localhost:5000' + targetContact.picture} alt="profile" /> :
+                                    <FontAwesomeIcon className={`user-icon user-icon-small ${targetContact.role}`}  icon={faUserAlt}/>} 
+                                    <h4>{targetContact.name}</h4>
+                                    {targetContact.online && <div className='online-dot'></div>}
                             </div>}
                         </header>
                         <main>
-                            { selectedConv && selectedConv.map((msg, i) => <div>{msg.text}</div>)
+                            {/* { selectedConv && selectedConv.map((msg, i) => <div>{msg.text}</div>)
 
-                            }
+                            } */}
                         </main>
                         <form onSubmit={sendMessage}>
                             <input type="text" id="message" name='message' autoComplete='off' value={msgInpu} onChange={handleChange}/>
