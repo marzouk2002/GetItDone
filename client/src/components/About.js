a2d9778124800e62d47d43a12ee07ee2054e0519
marzouk2002 <marzoukyoussouf2002@gmail.com>
2021-06-14 12:28:16 +0100

    initiale about page content

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -11,12 +11,81 @@ function About() {
                     <header className="major">
                         <h1>Landing</h1>
                     </header>
-                    <div className="content">
-                        <p>Lorem ipsum dolor sit amet nullam consequat<br />
-                        sed veroeros. tempus adipiscing nulla.</p>
-                    </div>
                 </div>
             </section>
+            <div id="main">
+                <section id="one">
+                    <div className="inner">
+                        <header className="major">
+                            <h2>Sed amet aliquam</h2>
+                        </header>
+                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna.</p>
+                    </div>
+                </section>
+
+                <section id="two" className="spotlights">
+                    <section>
+                        <a href="generic.html" className="image">
+                            <img src="images/pic08.jpg" alt="" data-position="center center" />
+                        </a>
+                        <div className="content">
+                            <div className="inner">
+                                <header className="major">
+                                    <h3>Orci maecenas</h3>
+                                </header>
+                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                                <ul className="actions">
+                                    <li><a href="generic.html" className="button">Learn more</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <a href="generic.html" className="image">
+                            <img src="images/pic09.jpg" alt="" data-position="top center" />
+                        </a>
+                        <div className="content">
+                            <div className="inner">
+                                <header className="major">
+                                    <h3>Rhoncus magna</h3>
+                                </header>
+                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                                <ul className="actions">
+                                    <li><a href="generic.html" className="button">Learn more</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                    <section>
+                        <a href="generic.html" className="image">
+                            <img src="images/pic10.jpg" alt="" data-position="25% 25%" />
+                        </a>
+                        <div className="content">
+                            <div className="inner">
+                                <header className="major">
+                                    <h3>Sed nunc ligula</h3>
+                                </header>
+                                <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
+                                <ul className="actions">
+                                    <li><a href="generic.html" className="button">Learn more</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </section>
+                </section>
+
+                <section id="three">
+                    <div class="inner">
+                        <header class="major">
+                            <h2>Massa libero</h2>
+                        </header>
+                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+                        <ul class="actions">
+                            <li><a href="generic.html" class="button next">Get Started</a></li>
+                        </ul>
+                    </div>
+                </section>
+			</div>
         </>
     )
 }
